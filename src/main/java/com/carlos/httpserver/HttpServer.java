c71445d0a2275d9476f0ce07c0e96e103b626433
CarlosLavin1 <carlosfranciscolavin@gmail.com>
2025-03-16 23:01:09 -0300

    configuration manager done

--- src/main/java/com/carlos/httpserver/HttpServer.java
+++ src/main/java/com/carlos/httpserver/HttpServer.java
@@ -1,10 +1,13 @@
 package com.carlos.httpserver;
 
 import com.carlos.httpserver.config.ConfigurationManager;
+import com.carlos.httpserver.config.Configuration;
 
 public class HttpServer {
     public static void main(String[] args) {
         System.out.println("starting...");
         ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
+        Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
+        System.out.println("using port: " + conf.getPort() + " using webroot: " + conf.getWebroot());
     }
 }
