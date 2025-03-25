42f468dd283426ed6427f1a154c02425c4edadc2
CarlosLavin1 <carlosfranciscolavin@gmail.com>
2025-03-24 23:52:18 -0300

    added logging

--- src/main/java/com/carlos/httpserver/HttpServer.java
+++ src/main/java/com/carlos/httpserver/HttpServer.java
@@ -3,6 +3,8 @@ package com.carlos.httpserver;
 import com.carlos.httpserver.config.ConfigurationManager;
 import com.carlos.httpserver.config.Configuration;
 import com.carlos.httpserver.core.ServerListenerThread;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,11 +13,12 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class HttpServer {
+    private final static Logger LOGGER = LoggerFactory.getLogger(HttpServer.class);
     public static void main(String[] args) {
-        System.out.println("starting...");
+        LOGGER.info("starting server...");
         ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
         Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
-        System.out.println("using port: " + conf.getPort() + " using webroot: " + conf.getWebroot());
+        LOGGER.info("using port: " + conf.getPort() + " using webroot: " + conf.getWebroot());
 
         try {
             ServerListenerThread serverListenerThread = new ServerListenerThread(conf.getPort(), conf.getWebroot());
